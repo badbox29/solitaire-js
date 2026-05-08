967b81acd7fd457ccb3681759d39ba2b73bb0cc4
badbox29 <96398618+badbox29@users.noreply.github.com>
2026-05-08 15:29:53 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1463,166 +1463,105 @@ Optional Features:
       }
 
    // throw confetti
-      /* Thanks to @gamanox
-         https://codepen.io/gamanox/pen/FkEbH
-      */
+      var confettiAnimFrame = null;
+
       function throwConfetti() {
          console.log('Confetti!');
 
-         var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, drawCircle2, drawCircle3, i, range, xpos;
+         var canvas = d.getElementById('confetti');
+         var ctx = canvas.getContext('2d');
 
-         NUM_CONFETTI = 60;
+         // cancel any existing animation
+         if (confettiAnimFrame) {
+            cancelAnimationFrame(confettiAnimFrame);
+            confettiAnimFrame = null;
+         }
 
-         COLORS = [[255, 255, 255], [255, 144, 0], [255, 255, 255], [255, 144, 0], [0, 277, 235]];
+         // size canvas to cover full viewport
+         canvas.width  = window.innerWidth;
+         canvas.height = window.innerHeight;
+         canvas.style.opacity = '1';
 
-         PI_2 = 2 * Math.PI;
+         var COLORS = [
+            '#f44336','#e91e63','#9c27b0','#673ab7',
+            '#3f51b5','#2196f3','#00bcd4','#009688',
+            '#4caf50','#cddc39','#ffeb3b','#ffc107',
+            '#ff9800','#ff5722','#ffffff','#00e5ff'
+         ];
 
-         canvas = d.getElementById("confetti");
+         var NUM   = 200;
+         var DURATION = 5000;
+         var startTime = null;
+         var pieces = [];
 
-         context = canvas.getContext("2d");
+         for (var i = 0; i < NUM; i++) {
+            pieces.push({
+               x:     Math.random() * canvas.width,
+               y:     Math.random() * canvas.height - canvas.height,
+               w:     Math.random() * 14 + 6,
+               h:     Math.random() * 7  + 4,
+               color: COLORS[Math.floor(Math.random() * COLORS.length)],
+               vx:    Math.random() * 4 - 2,
+               vy:    Math.random() * 4 + 1.5,
+               angle: Math.random() * Math.PI * 2,
+               spin:  (Math.random() - 0.5) * 0.25,
+               wobble: Math.random() * Math.PI * 2,
+               wobbleSpeed: Math.random() * 0.05 + 0.02
+            });
+         }
 
-         window.w = 0;
+         function animate(timestamp) {
+            if (!startTime) startTime = timestamp;
+            var elapsed  = timestamp - startTime;
+            var progress = Math.min(elapsed / DURATION, 1);
 
-         window.h = 0;
+            // fade out in final 25%
+            var fadeStart = 0.75;
+            var alpha = progress > fadeStart
+               ? 1 - (progress - fadeStart) / (1 - fadeStart)
+               : 1;
 
-         window.resizeWindow = function() {
-            window.w = canvas.width = window.innerWidth;
-            return window.h = canvas.height = window.innerHeight;
-         };
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-         window.addEventListener('resize', resizeWindow, false);
+            for (var i = 0; i < pieces.length; i++) {
+               var p = pieces[i];
 
-         window.onload = function() {
-            return setTimeout(resizeWindow, 0);
-         };
+               p.wobble += p.wobbleSpeed;
+               p.x  += p.vx + Math.sin(p.wobble) * 0.8;
+               p.y  += p.vy;
+               p.vy += 0.04; // gravity
+               p.angle += p.spin;
 
-         range = function(a, b) {
-            return (b - a) * Math.random() + a;
-         };
+               // wrap horizontally
+               if (p.x < -p.w)              p.x = canvas.width  + p.w;
+               if (p.x > canvas.width  + p.w) p.x = -p.w;
 
-         drawCircle = function(x, y, r, style) {
-            context.beginPath();
-            context.moveTo(x, y);
-            context.bezierCurveTo(x - 17, y + 14, x + 13, y + 5, x - 5, y + 22);
-            context.lineWidth = 3;
-            context.strokeStyle = style;
-            return context.stroke();
-         };
-
-         drawCircle2 = function(x, y, r, style) {
-            context.beginPath();
-            context.moveTo(x, y);
-            context.lineTo(x + 10, y + 10);
-            context.lineTo(x + 10, y);
-            context.closePath();
-            context.fillStyle = style;
-            return context.fill();
-         };
-
-         drawCircle3 = function(x, y, r, style) {
-            context.beginPath();
-            context.moveTo(x, y);
-            context.lineTo(x + 10, y + 10);
-            context.lineTo(x + 10, y);
-            context.closePath();
-            context.fillStyle = style;
-            return context.fill();
-         };
-
-         xpos = 0.9;
-
-         d.onmousemove = function(e) {
-            return xpos = e.pageX / w;
-         };
-
-         window.requestAnimationFrame = (function() {
-            return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
-                  return window.setTimeout(callback, 100 / 20);
-            };
-         })();
-
-         Confetti = (function() {
-            function Confetti() {
-               this.style = COLORS[~~range(0, 5)];
-               this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
-               this.r = ~~range(2, 6);
-               this.r2 = 2 * this.r;
-               this.replace();
-            }
-
-            Confetti.prototype.replace = function() {
-               this.opacity = 0;
-               this.dop = 0.03 * range(1, 4);
-               this.x = range(-this.r2, w - this.r2);
-               this.y = range(-20, h - this.r2);
-               this.xmax = w - this.r;
-               this.ymax = h - this.r;
-               this.vx = range(0, 2) + 8 * xpos - 5;
-               return this.vy = 0.7 * this.r + range(-1, 1);
-            };
-
-            Confetti.prototype.draw = function() {
-               var ref;
-               this.x += this.vx;
-               this.y += this.vy;
-               this.opacity += this.dop;
-               if (this.opacity > 1) {
-                 this.opacity = 1;
-                 this.dop *= -1;
+               // recycle from top when off bottom
+               if (p.y > canvas.height + p.h) {
+                  p.y  = -p.h;
+                  p.x  = Math.random() * canvas.width;
+                  p.vy = Math.random() * 4 + 1.5;
                }
-               if (this.opacity < 0 || this.y > this.ymax) {
-                 this.replace();
-               }
-               if (!((0 < (ref = this.x) && ref < this.xmax))) {
-                 this.x = (this.x + this.xmax) % this.xmax;
-               }
-               drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
-               drawCircle3(~~this.x * 0.5, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
-               return drawCircle2(~~this.x * 1.5, ~~this.y * 1.5, this.r, this.rgb + "," + this.opacity + ")");
-            };
 
-            return Confetti;
-
-         })();
-
-         confetti = (function() {
-            var j, ref, results;
-            results = [];
-            for (i = j = 1, ref = NUM_CONFETTI; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
-               results.push(new Confetti);
+               ctx.save();
+               ctx.globalAlpha = alpha;
+               ctx.translate(p.x, p.y);
+               ctx.rotate(p.angle);
+               ctx.fillStyle = p.color;
+               ctx.fillRect(-p.w / 2, -p.h / 2, p.w, p.h);
+               ctx.restore();
             }
-            return results;
-         })();
 
-         window.step = function() {
-            var c, j, len, results;
-            requestAnimationFrame(step);
-            context.clearRect(0, 0, w, h);
-            results = [];
-            for (j = 0, len = confetti.length; j < len; j++) {
-               c = confetti[j];
-               results.push(c.draw());
+            if (progress < 1) {
+               confettiAnimFrame = requestAnimationFrame(animate);
+            } else {
+               canvas.style.opacity = '0';
+               ctx.clearRect(0, 0, canvas.width, canvas.height);
+               confettiAnimFrame = null;
             }
-            return results;
-         };
+         }
 
-         step();
-
-         // fix initial bug when firing
-         resizeWindow();
-
-         // fade in
-         canvas.style.opacity = 0;
-         var tick = function() {
-            canvas.style.opacity = +canvas.style.opacity + 0.01;
-            if ( +canvas.style.opacity < 1 ) {
-               ( window.requestAnimationFrame &&
-                 requestAnimationFrame(tick) ) ||
-               setTimeout(tick, 100)
-            }
-         };
-         tick();
-
+         confettiAnimFrame = requestAnimationFrame(animate);
       }
 
 // ### NEW GAME ###
@@ -1664,6 +1603,10 @@ Optional Features:
       $autoWin.removeEventListener('click', autoWin);
 
       // clear confetti canvas
+      if (confettiAnimFrame) {
+         cancelAnimationFrame(confettiAnimFrame);
+         confettiAnimFrame = null;
+      }
       var confettiCanvas = d.getElementById('confetti');
       confettiCanvas.style.opacity = 0;
       var confettiCtx = confettiCanvas.getContext('2d');
